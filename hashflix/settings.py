055d6c2da637a75b098316ca5c953f7f7271d4d2
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-10-20 17:33:34 -0300

    ajuste ALLOWED_HOSTS

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -33,7 +33,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hashflix-production-0f98.up.railway.app/', "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://hashflix-production-0f98.up.railway.app/", "localhost", "127.0.0.1"]
 
 
 # Application definition
