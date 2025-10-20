8181c19392a36f57b4a0746014d65fb43d40e087
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-10-20 16:35:54 -0300

    ajustes

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv('DATABASE_URL')
 # se estiver online, a variavel de ambiente terá valor
 if DATABASE_URL:
     DATABASES = {
