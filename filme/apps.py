08ed44d2c325219b1b5cc709d45827aaa4a23410
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-10-20 20:44:38 -0300

    ajuste

--- filme/apps.py
+++ filme/apps.py
@@ -12,6 +12,6 @@ class FilmeConfig(AppConfig):
         email = os.getenv("EMAIL_ADMIN")
         senha = os.getenv("SENHA_ADMIN")
 
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
+        usuario = Usuario.objects.filter(email=email)
+        if not usuario:
             Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
