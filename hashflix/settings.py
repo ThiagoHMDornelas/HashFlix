dc2a6c1c4290eb7f4f20c15d72eee3dad738df29
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-10-20 16:46:05 -0300

    ajustes de banco

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -91,7 +91,10 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+#DATABASE_URL = os.getenv('DATABASE_URL')
+
+DATABASE_URL = 'postgresql://postgres:VoqUIuCyEBPrkPuBJsgQtlbulkbBTyBZ@postgres.railway.internal:5432/railway'
+
 # se estiver online, a variavel de ambiente terá valor
 if DATABASE_URL:
     DATABASES = {
