26db30aff81d7c96154e266b32106dded86cd111
Thiago Dornelas <thiagohmdornelas@gmail.com>
2025-10-22 11:22:12 -0300

    debug false

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -35,7 +35,7 @@ if os.getenv('TOKEN_CSRF'):
         'https://hashflix-production-98fb.up.railway.app',
         'https://*.up.railway.app',  # adiciona suporte a subdomínios do Railway
     ]
-    DEBUG = True
+    DEBUG = False
     # Hosts permitidos
     ALLOWED_HOSTS = [
         'hashflix-production-98fb.up.railway.app',
